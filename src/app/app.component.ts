f554820a8daa7e6869ac4c0c84202e59927a69d3
Luka <luka.akhalkatsi.1@btu.edu.ge>
2025-02-01 23:47:46 -0800

    global styling

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { PigComponent } from './pig/pig.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [PigComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
-export class AppComponent {
-  title = 'frontend';
-}
+export class AppComponent {}
---
+++ src/app/pig/pig.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+@Component({
+  selector: 'app-pig',
+  standalone: true,
+  imports: [],
+  templateUrl: './pig.component.html',
+  styleUrl: './pig.component.css',
+})
+export class PigComponent {
+  imageUrl: string = 'assets/images/pig.jpg';
+}
